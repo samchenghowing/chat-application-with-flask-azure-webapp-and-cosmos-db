b6e06e96c603c9453f24cd727c81f72dfce846f3
to-dare <90386822+to-dare@users.noreply.github.com>
2023-04-07 08:46:54 +0800

    Create Home.vue

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,19 @@
+<!-- <template>
+  <v-container>
+
+  </v-container>
+</template> -->
+<template>
+  <div>
+    <h1>Welcome to my chat website!</h1>
+    <p>Here you can chat with your classmates in real-time.</p>
+    <router-link to="/login">Log in</router-link>
+    <router-link to="/register">Register</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
