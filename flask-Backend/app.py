0c7aba88eb0a7564ac17c812bdd0e0daf5084adc
Chuenwyc <wyc0035@gmail.com>
2023-04-07 09:40:26 +0000

    backend

--- flask-Backend/app.py
+++ flask-Backend/app.py
@@ -1,12 +1,21 @@
 import os
 import socket
 from flask import Flask,request,jsonify
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route("/")
 def main():
-    return "Welcome Python flask!"
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return str(posts) 
 
 @app.route('/api/about')
 def about():
