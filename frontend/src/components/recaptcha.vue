4b6f335838725fb62dd1d98016d2fa09d344c2c0
Chuenwyc <66900465+Chuenwyc@users.noreply.github.com>
2023-04-12 14:14:44 +0000

    Add recaptcha

---
+++ frontend/src/components/recaptcha.vue
@@ -0,0 +1,39 @@
+<template>
+  <VueRecaptcha
+    :sitekey="6Ld40X8lAAAAAH_jjgZpzjTV-OFZd48Mq-quUD3Z"
+    :load-recaptcha-script="true"
+    @verify="handleSuccess"
+    @error="handleError"
+  ></VueRecaptcha>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { VueRecaptcha } from 'vue-recaptcha';
+
+export default defineComponent({
+  name: 'ReCaptcha',
+  components: {
+    VueRecaptcha
+  },
+  setup() {
+    const siteKey = computed(() => {
+      return 'yourSiteAPIKey';
+    });
+
+    const handleError = () => {
+      // Do some validation
+    };
+
+    const handleSuccess = (response: string) => {
+     // Do some validation
+    };
+
+    return {
+      handleSuccess,
+      handleError,
+      siteKey,
+    };
+  }
+});
+</script>
