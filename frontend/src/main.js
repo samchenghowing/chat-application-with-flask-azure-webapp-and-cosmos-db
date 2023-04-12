e9db319bca4b27f51eaa592c8cfdb82a8d9fad8d
Chuenwyc <66900465+Chuenwyc@users.noreply.github.com>
2023-04-12 17:45:21 +0000

    adjust reCaptcha

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { VueReCaptcha } from 'vue-recaptcha-v3'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from './router'
@@ -7,6 +8,9 @@ import router from './router'
 loadFonts()
 
 createApp(App)
+  .use(store)
   .use(router)
   .use(vuetify)
+  .use(VueReCaptcha, { siteKey: '6Lf-EIAlAAAAAN0-gJgvH3jfJnR2ti1Trb1UGxtp' })
   .mount('#app')
+  
